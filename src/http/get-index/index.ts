36ceae2638dc3442852276b1781e5af7e169dba3
swyx <shawnthe1@gmail.com>
2020-06-01 16:19:29 +0800

    body

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,51 +1,51 @@
-// TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
+// // TODO: modify the body object!
+// let body = `
+// <!doctype html>
+// <html lang=en>
+//   <head>
+//     <meta charset=utf-8>
+//     <title>Hi!</title>
+//     <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
+//     <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
+//   </head>
+//   <body>
 
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
+//     <h1 class="center-text">
+//       <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
+//       Hello world!
+//     </h1>
 
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
+//     <p class="center-text">
+//       Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
+//     </p>
 
-  </body>
-</html>
-`
+//   </body>
+// </html>
+// `;
 
-export async function handler (req: object) {
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    body
-  }
-}
+// export async function handler (req: object) {
+//   return {
+//     headers: {
+//       'content-type': 'text/html; charset=utf8',
+//       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+//     },
+//     body
+//   }
+// }
 
 // Example responses
 
-/* Forward requester to a new path
-export async function handler (req: object) {
+// Forward requester to a new path
+export async function handler(req: object) {
   return {
     statusCode: 302,
     headers: {
-      'location': '/about',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    }
-  }
+      location: "https://crackingthecodingcareer.com",
+      "cache-control":
+        "no-cache, no-store, must-revalidate, max-age=0, s-maxage=0",
+    },
+  };
 }
-*/
 
 /* Respond with successful resource creation
 export async function handler (req: object) {
