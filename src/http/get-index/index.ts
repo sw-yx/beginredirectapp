643690c6d39b5dc05a2c4abb41402212426f4a3e
swyx <shawnthe1@gmail.com>
2020-07-03 08:51:13 +0800

    try fix

--- src/http/get-index/index.ts
+++ src/http/get-index/index.ts
@@ -1,6 +1,6 @@
 
 // Forward requester to a new path
-export async function handler(req: object) {
+export async function handler(req: {queryStringParameters}) {
   let location = "https://www.learninpublic.org"
   let qsp = req.queryStringParameters
   if (qsp && qsp.a) location += `/?a=${qsp.a}`
